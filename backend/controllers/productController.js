2a96cdc874a85eaf743f3d21dec06d7be6d2608b
wailinse4 <wailinse4@gmail.com>
2025-02-10 06:14:01 -0500

    feat: Add get products feature

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -43,3 +43,14 @@ export const updateProduct = async (req, res) => {
         res.status(500).json({ message: "Internal Server Error" })
     }  
 }
+
+export const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json({ message: "Products Found", data: products })
+    }
+    catch(error) {
+        console.error(error)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createProduct, deleteProduct, updateProduct } from "../controllers/productController.js"
+import { createProduct, deleteProduct, updateProduct, getProducts } from "../controllers/productController.js"
 
 const router = express.Router()
 
 router.post("/", createProduct)
 router.delete("/:productId", deleteProduct)
 router.patch("/:productId", updateProduct)
+router.get("/", getProducts)
 
 export default router
