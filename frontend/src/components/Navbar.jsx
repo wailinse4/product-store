0bc17861a6ceabc0bfc7998aef9e2cd5239e7391
wailinse4 <wailinse4@gmail.com>
2025-02-10 12:15:21 -0500

    feat: Add basic navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
+        <div className="max-w-7xl mx-auto">
+            <div className="navbar px-4 min-h-[4rem] justify-between">
+                { /* LOGO */ }
+                <div className="flex-1 lg:flex-none">
+                    <span className="font-semibold font-mono tracking-widest text-2xl bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">Product Store</span>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
 
 export default Navbar
+
