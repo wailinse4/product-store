878fd1f6e069b263024ef38d6bd19ceafb79af19
wailinse4 <wailinse4@gmail.com>
2025-02-10 15:27:49 -0500

    feat: Add data fetching to home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useEffect } from "react"
+import { useProductStore } from "../store/useProductStore.js"
 
 const HomePage = () => {
+  const { products, loading, error, fetchProducts } = useProductStore()
+  useEffect(() => {
+    fetchProducts()
+  }, [fetchProducts])
+
+  console.log(products)
+
   return (<div>Home Page</div>)
 }
 
