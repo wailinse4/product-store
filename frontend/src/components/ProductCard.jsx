e9da57fa6899a46370c7c2aef7878d2ca37e1d1b
wailinse4 <wailinse4@gmail.com>
2025-02-10 18:31:51 -0500

    feat: Add delete and update icons to product card

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Link } from "react-router-dom" 
+import { EditIcon, Trash2Icon } from "lucide-react";
+
+import { useProductStore } from '../store/useProductStore'; // new 
 
 const ProductCard = ({product}) => {
+
+const { deleteProduct } = useProductStore() // new 
+
   return (
     <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300">
         {/* PRODUCT IMAGE */}
@@ -16,6 +23,19 @@ const ProductCard = ({product}) => {
             {/* PRODUCT INFO */}
             <h2 className="card-title text-lg font-semibold">{product.name}</h2>
             <p className="text-2xl font-bold text-primary">${Number(product.price).toFixed(2)}</p>
+
+            {/* CARD ACTIONS */}
+            <div className="card-actions justify-end mt-4">
+                <Link to={`/product/${product._id}`} className="btn btn-sm btn-info btn-outline">
+                    <EditIcon className="size-4" />
+                </Link>
+
+                <button
+                    className="btn btn-sm btn-error  btn-outline"
+                    onClick={() => deleteProduct(product._id)}>
+                    <Trash2Icon className="size-4" />
+                </button>
+            </div>
         </div>
     </div>
   )
