60f31b49d4af85e2118da573cb3d3aef9966e1e2
wailinse4 <wailinse4@gmail.com>
2025-02-09 21:56:25 -0500

    feat: Create basic express server

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`The server is listening on port ${port}`))
