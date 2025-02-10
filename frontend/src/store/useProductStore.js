7265f648077fc763767c5ac4ecfa22fcefee7e0b
wailinse4 <wailinse4@gmail.com>
2025-02-10 15:22:14 -0500

    feat: Add data fetching to product store

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -1,4 +1,5 @@
 import { create } from "zustand"
+import axios from "axios"
 
 const BASE_URL = "http://localhost:3000"
 
@@ -6,4 +7,19 @@ export const useProductStore = create((set, get) => ({
     products: [], 
     loading: false, 
     error: null, 
+
+    fetchProducts: async () => {
+        set({ loading: true, error: null })
+
+        try {
+            const response = await axios.get(`${BASE_URL}/api/products`)
+            set({ products: response.data.data, error: null }) // response.data? 
+        }
+        catch(error) {
+            set({ error: error.message })
+        }
+        finally {
+            set({ loading: false })
+        }
+    }
 }))
