dad4c76c24cd022d9fb0e37e7b9524d59538c614
wailinse4 <wailinse4@gmail.com>
2025-02-10 16:24:51 -0500

    feat: Implement product card component

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = ({product}) => {
   return (
-    <div>
-        Product
+    <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300">
+        {/* PRODUCT IMAGE */}
+        <figure className="relative pt-[56.25%]">
+            <img
+            src={product.image}
+            alt={product.name}
+            className="absolute top-0 left-0 w-full h-full object-cover"
+            />
+        </figure>
+
+        <div className="card-body">
+            {/* PRODUCT INFO */}
+            <h2 className="card-title text-lg font-semibold">{product.name}</h2>
+            <p className="text-2xl font-bold text-primary">${Number(product.price).toFixed(2)}</p>
+        </div>
     </div>
   )
 }
