f38f09395db5cbbbfec28c103d6f921d43091758
wailinse4 <wailinse4@gmail.com>
2025-02-10 11:58:43 -0500

    feat: Configure react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,20 @@
+import { Routes, Route } from "react-router-dom"
+
+import Navbar from "./components/Navbar.jsx"
+
+import HomePage from "./pages/HomePage.jsx"
+import ProductPage from "./pages/ProductPage.jsx"
+
 function App() {
   return (
-    <h1 className="text-red-200">Hello, World!</h1>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/product/:id" element={<ProductPage />}></Route>
+      </Routes>
+    </div>
   )
 }
 export default App
