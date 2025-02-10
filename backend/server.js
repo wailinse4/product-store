e415f3ec15e643e8bba98fe18c74e84018619375
wailinse4 <wailinse4@gmail.com>
2025-02-10 06:26:22 -0500

    feat: Add security and logging middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
+import helmet from "helmet"
+import cors from "cors"
+import morgan from "morgan"
 
 import connectDB from "./config/db.js"
 
@@ -10,8 +13,11 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(helmet())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(morgan("dev"))
 
 app.use("/api/products", productRoutes)
 
